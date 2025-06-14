8a6241b85cd3c8d06df4d919dba2242879e09f72
Cengiz <ata.kaan.cengiz@outlook.de>
2025-06-15 01:25:24 +0200

    fertig

--- src/A3_5/BookTest.java
+++ src/A3_5/BookTest.java
@@ -14,6 +14,9 @@ public class BookTest {
     @Test
     public void testToString() {
         Book book = new Book("Joshua Bloch", "Effective Java");
+        //die ausgabe ist umgeschrieben weil er in der ersten Aufgabe Joshua Bloch: Effective Java also keine "" haben möchte
+        //aber dann will der hund es später in der aufgabe so haben Joshua Bloch: "Effective Java"
+        //deswegen nicht wundern
         String expected = "Joshua Bloch: \"Effective Java\"";
         assertEquals(expected, book.toString());
     }
